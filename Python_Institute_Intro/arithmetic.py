f3b7d8e7a09a0ec689a0491fb03bc09f1a0abe2d
Kristina Ilyovska <lyloster@gmail.com>
2020-08-11 16:18:28 -0400

    Some calculations

---
+++ Python_Institute_Intro/arithmetic.py
@@ -0,0 +1,13 @@
+# input a float value for variable a here
+f1 = float(input("Please enter a number: "))
+# input a float value for variable b here
+f2 = float(input("Please enter a number: "))
+# output the result of addition here
+print("f1 + f2 = ", f1 + f2)
+# output the result of subtraction here
+print("f1 - f2 = ", f1 - f2)
+# output the result of multiplication here
+print("f1 * f2 = ", f1 * f2)
+# output the result of division here
+print("f1 / f2 = ", f1 / f2)
+print("\nThat's all, folks!")
