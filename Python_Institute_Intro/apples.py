4b17fdf1d66178d05e237d6732493398c1edd676
Kristina Ilyovska <lyloster@gmail.com>
2020-08-11 15:48:25 -0400

    Intro

---
+++ Python_Institute_Intro/apples.py
@@ -0,0 +1,6 @@
+john = 3
+mary = 5
+adam = 6
+print(john, mary, adam, sep = ', ')
+totalApples = john + mary + adam
+print("Total number of apples is: ", totalApples)
---
+++ Python_Institute_Intro/digits.py
@@ -0,0 +1,2 @@
+print(0o123) #octal
+print(0x123) # hexadecimal
